c0a0ad06e8734895878632ee12fadfd2fe535b79
RobottiFranco <franco.robotti0@gmail.com>
2025-02-16 16:31:34 -0300

    imprimir

---
+++ imprimir.py
@@ -0,0 +1,7 @@
+from imprimirConsum import graficoBloqueo
+
+
+archivos_uy = [ "src/data/comparative/200.58.141.25.csv",
+                "src/data/comparative/200.40.53.104.csv"]
+
+graficoBloqueo(archivos_uy)
