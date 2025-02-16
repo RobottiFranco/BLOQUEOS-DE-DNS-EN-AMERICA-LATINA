d193b16348477eb78d311d45da25eb03658e8abe
RobottiFranco <franco.robotti0@gmail.com>
2025-02-16 14:53:55 -0300

    update

--- mesh_system.py
+++ mesh_system.py
@@ -8,5 +8,5 @@ def mesh(directory: str) -> None:
 def delete_url(directory, output_file) -> None:
     output_file = delete_no_exist_files(directory, output_file)
 
-""" mesh("src/data/comparative") """
+mesh("src/data/comparative")
 delete_url("src/data/comparative","src/data/comparative/pages_no_longer_exist.csv")
